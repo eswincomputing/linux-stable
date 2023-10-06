fd9243811d0c152161d2102e52df7fc9fe4d35fc
Mario Limonciello <mario.limonciello@amd.com>
2023-10-06 13:50:22 -0500

    fix:amdgpu suspend Changelogs: drm/amd: Split up UVD suspend into prepare and suspend steps
    amdgpu_uvd_suspend() allocates memory and copies objects into that
    allocated memory.  This fails under memory pressure.  Instead move
    majority of this code into a prepare step when swap can still be
    allocated.
    
    Reviewed-by: Christian König <christian.koenig@amd.com>
    Signed-off-by: Mario Limonciello <mario.limonciello@amd.com>
    Signed-off-by: Alex Deucher <alexander.deucher@amd.com>

--- drivers/gpu/drm/amd/amdgpu/uvd_v3_1.c
+++ drivers/gpu/drm/amd/amdgpu/uvd_v3_1.c
@@ -727,6 +727,13 @@ static int uvd_v3_1_hw_fini(void *handle)
 	return 0;
 }
 
+static int uvd_v3_1_prepare_suspend(void *handle)
+{
+	struct amdgpu_device *adev = (struct amdgpu_device *)handle;
+
+	return amdgpu_uvd_prepare_suspend(adev);
+}
+
 static int uvd_v3_1_suspend(void *handle)
 {
 	int r;
@@ -827,6 +834,7 @@ static const struct amd_ip_funcs uvd_v3_1_ip_funcs = {
 	.sw_fini = uvd_v3_1_sw_fini,
 	.hw_init = uvd_v3_1_hw_init,
 	.hw_fini = uvd_v3_1_hw_fini,
+	.prepare_suspend = uvd_v3_1_prepare_suspend,
 	.suspend = uvd_v3_1_suspend,
 	.resume = uvd_v3_1_resume,
 	.is_idle = uvd_v3_1_is_idle,
