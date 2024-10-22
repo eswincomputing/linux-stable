f6b43e12f04f607cb6074c9db9a0b2f7a88d60b9
tangdaoyong <tangdaoyong@eswincomputing.com>
2024-10-22 09:35:00 +0800

    feat:add license description
    Changelogs:
      add license description.
    
    Signed-off-by: tangdaoyong <tangdaoyong@eswincomputing.com>

--- drivers/staging/media/eswin/vdec/vdec_allocator.h
+++ drivers/staging/media/eswin/vdec/vdec_allocator.h
@@ -1,3 +1,23 @@
+// SPDX-License-Identifier: GPL-2.0
+/*
+ * ESWIN video decoder driver
+ *
+ * Copyright 2024, Beijing ESWIN Computing Technology Co., Ltd.. All rights reserved.
+ * SPDX-License-Identifier: GPL-2.0
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, version 2.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ *
+ */
 #ifndef __VDEC_ALLOCATOR__H
 #define __VDEC_ALLOCATOR__H
 
