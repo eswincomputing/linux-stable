4eb29b9552b2b8eed4a988ff56202ee18cc8c97b
yangqiang <yangqiang1@eswincomputing.com>
2024-11-12 10:49:47 +0800

    feat:modify the audio proc license statement.
    Changelogs:
    	modify the audio proc license statement.
    
    Signed-off-by: yangqiang <yangqiang1@eswincomputing.com>

--- sound/soc/eswin/esw-audio-proc.h
+++ sound/soc/eswin/esw-audio-proc.h
@@ -1,21 +1,26 @@
+// SPDX-License-Identifier: GPL-2.0
 /*
+ * ESWIN audio proc driver
  *
- * Copyright (C) 2021 ESWIN, Inc. All rights reserved.
+ * Copyright 2024, Beijing ESWIN Computing Technology Co., Ltd.. All rights reserved.
  *
- * This program is free software; you can redistribute it and/or modify
+ * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; either version 2 of the License, or
- * (at your option) any later version.
+ * the Free Software Foundation, version 2.
  *
- * This program is distributed in the hope that it will be useful, but WITHOUT
- * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
- * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for
- * more details.
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
  *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ *
+ * Authors: Zack Yang <yangqiang1@eswincomputing.com>
  */
 
-#ifndef _ES_AUDIO_PROC_H_
-#define _ES_AUDIO_PROC_H_
+#ifndef _ES_AUDIO_PROC_H
+#define _ES_AUDIO_PROC_H
 
 int audio_proc_module_init(void);
 void audio_proc_module_exit(void);
