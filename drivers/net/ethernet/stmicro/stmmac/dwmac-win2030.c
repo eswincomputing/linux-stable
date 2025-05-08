e03387e361fcd86d61ffb638d8e7dd536fb4faed
lizhi2 <lizhi2@eswincomputing.com>
2025-05-08 13:54:20 +0800

    feat(eth): ETH WOL support in EIC7702_D560
    Changelogs:
    1. support WOL func by configuring PHY reg.
    
    Signed-off-by: lizhi2 <lizhi2@eswincomputing.com>

--- drivers/net/ethernet/stmicro/stmmac/dwmac-win2030.c
+++ drivers/net/ethernet/stmicro/stmmac/dwmac-win2030.c
@@ -226,7 +226,7 @@ static int dwc_eth_dwmac_config_dt(struct platform_device *pdev,
 	plat_dat->has_gmac4 = 1;
 	plat_dat->dma_cfg->aal = 1;
     plat_dat->flags |= STMMAC_FLAG_TSO_EN;
-	plat_dat->pmt = 1;
+	plat_dat->pmt = 0;
 
 	return 0;
 }
@@ -735,7 +735,7 @@ static struct platform_driver win2030_eth_dwmac_driver = {
 	.shutdown = dwc_eth_dwmac_shutdown,
 	.driver = {
 		.name           = "win2030-eth-dwmac",
-		.pm             = &stmmac_pltfr_pm_ops,
+		.pm             = pm_sleep_ptr(&stmmac_pltfr_pm_ops),
 		.of_match_table = dwc_eth_dwmac_match,
 	},
 };
