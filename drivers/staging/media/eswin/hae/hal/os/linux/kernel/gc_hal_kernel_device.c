08a473239e1647bf5b1982b44e48468fbec58a37
lei zhilin <leizhilin@eswincomputing.com>
2025-05-13 14:37:57 +0800

    Revert "<fix>(2d):hae power off delay by timeout"
    This reverts commit ba8cd3fad9fbee53ef7d7f3f02e1d051f0e73ee4.
    
    Reason for revert: <INSERT REASONING HERE> smoke failed

--- drivers/staging/media/eswin/hae/hal/os/linux/kernel/gc_hal_kernel_device.c
+++ drivers/staging/media/eswin/hae/hal/os/linux/kernel/gc_hal_kernel_device.c
@@ -2268,8 +2268,7 @@ isrRoutine(int irq, void *ctxt)
     }
 
     if(status == gcvSTATUS_GENERIC_IO){
-        hae_print("irq: %d, hw: %p, addr: %llx clock close.\n",
-            irq, kernel->hardware, kernel->hardware->lastExecuteAddress);
+        pr_info("[%s] hae isr meet clock close...\n", __func__);
     }
 
     return IRQ_NONE;
