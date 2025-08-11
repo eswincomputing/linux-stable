966870957aa972fed84cf75ea22ea967b13bcf4f
lei zhilin <leizhilin@eswincomputing.com>
2025-08-11 15:29:25 +0800

    feat(2d):hae use opp table freq
    Changelogs:
        1.system need hae use opp table freq rather than cycle cacul frequency, it will cause system ipa controll err;
        2.optimize hae devfeq code;
    
    Signed-off-by: lei zhilin <leizhilin@eswincomputing.com>

--- drivers/staging/media/eswin/hae/hal/os/linux/kernel/gc_hal_kernel_device.h
+++ drivers/staging/media/eswin/hae/hal/os/linux/kernel/gc_hal_kernel_device.h
@@ -161,7 +161,7 @@ gckGALDEVICE_Construct(gcsPLATFORM *Platform, const gcsMODULE_PARAMETERS *Args, 
 gceSTATUS
 gckGALDEVICE_Destroy(gckGALDEVICE Device);
 
-int gckGALDEVICE_GetDevFreqInfo(struct device *dev, struct devfreq_dev_status *stat);
+int gckGALDEVICE_GetHardwareLoad(struct device *dev, struct devfreq_dev_status *stat);
 
 static gcmkINLINE gckKERNEL
 _GetValidKernel(gckGALDEVICE Device)
