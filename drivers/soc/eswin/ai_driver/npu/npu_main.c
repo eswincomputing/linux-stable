72c7af1843654f142061978317611879a45c77c6
zhuxuefei <zhuxuefei@eswincomputing.com>
2025-08-25 17:48:33 +0800

    feat:delete act_freq_level.
        Changelogs:
        1. delete act_freq_level.
    
    Signed-off-by: zhuxuefei <zhuxuefei@eswincomputing.com>

--- drivers/soc/eswin/ai_driver/npu/npu_main.c
+++ drivers/soc/eswin/ai_driver/npu/npu_main.c
@@ -673,8 +673,6 @@ static int32_t edla_probe(struct platform_device *pdev)
 		}
         mdelay(10);
 		err = npu_set_freq_req(nvdla_dev, &npu_freq_tbl[nvdla_dev->numa_id][NPU_1_5_GHZ]);
-		nvdla_dev->act_freq_level = 4;
-
     } else {
 		err = regulator_set_voltage(nvdla_dev->npu_regulator,
 						npu_freq_tbl[nvdla_dev->numa_id][NPU_1_0_GHZ].volt,
@@ -682,7 +680,6 @@ static int32_t edla_probe(struct platform_device *pdev)
 		dla_debug("name:%s, volt:%d, ret:%d\n", pdev->name, npu_freq_tbl[nvdla_dev->numa_id][NPU_1_0_GHZ].volt, err);
 		mdelay(10);
 		err = npu_set_freq_req(nvdla_dev, &npu_freq_tbl[nvdla_dev->numa_id][NPU_1_0_GHZ]);
-		nvdla_dev->act_freq_level = 3;
     }
 
 	if (err) {
@@ -918,7 +915,6 @@ int __maybe_unused npu_runtime_suspend(struct device *dev)
 	}
 
 	npu_tbu_power(dev, false);
-	ndev->act_freq_level = 0;
 
 	return npu_disable_clock(ndev);
 }
@@ -942,7 +938,6 @@ int __maybe_unused npu_runtime_resume(struct device *dev)
 
 	for (int i = 0; i < NPU_TBL_MAX; i++) {
 		if (npu_freq_tbl[ndev->numa_id][i].npu_rate == ndev->rate) {
-			ndev->act_freq_level = NPU_TBL_MAX - i;
 			break;
 		}
 	}
