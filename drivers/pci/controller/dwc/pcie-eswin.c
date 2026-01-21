a7e2e7abac358c2017b364d85bd0901526b36f6e
ouyang hui <ouyanghui@eswincomputing.com>
2026-01-21 10:31:30 +0800

    Revert "fix:amdgpu s3 rcu"
    This reverts commit bd4bab97b38bb3cce05f5386e6f75845ab06b03a.
    
    Reason for revert: <INSERT REASONING HERE>

--- drivers/pci/controller/dwc/pcie-eswin.c
+++ drivers/pci/controller/dwc/pcie-eswin.c
@@ -52,7 +52,6 @@ struct eswin_pcie {
 	struct reset_control *powerup_rst;
 	struct reset_control *cfg_rst;
 	struct reset_control *perst;
-	bool suspended;
 };
 
 #define PCIE_PM_SEL_AUX_CLK		BIT(16)
@@ -424,17 +423,12 @@ static int eswin_pcie_suspend(struct device *dev)
 	struct eswin_pcie *pcie = dev_get_drvdata(dev);
 
 	dev_dbg(dev, "%s\n", __func__);
-	/*
-	 * For controllers with active devices, resources are retained and
-	 * cannot be turned off.
-	 */
-	if (!dw_pcie_link_up(&pcie->pci)) {
-		pm_runtime_put_sync(dev);
-		win2030_tbu_power(dev, false);
-		eswin_pcie_power_off(pcie);
-		eswin_pcie_clk_disable(pcie);
-		pcie->suspended = true;
-	}
+
+	pm_runtime_put_sync(dev);
+	win2030_tbu_power(dev, false);
+	eswin_pcie_power_off(pcie);
+	eswin_pcie_clk_disable(pcie);
+	msleep(100);
 
 	return 0;
 }
@@ -445,9 +439,6 @@ static int eswin_pcie_resume(struct device *dev)
 	int ret, retry_times = 0;
 
 	dev_dbg(dev, "%s\n", __func__);
-
-	if (!pcie->suspended)
-		return 0;
 
 	pm_runtime_get_sync(dev);
 	ret = eswin_pcie_host_init(&pcie->pci.pp);
@@ -470,8 +461,6 @@ static int eswin_pcie_resume(struct device *dev)
 		dev_err(dev, "Phy link never came up\n");
 		return -ETIMEDOUT;
 	}
-
-	pcie->suspended = false;
 
 	return 0;
 }
