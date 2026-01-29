6b87d115e9685827fe4d17f20152d06a3c373113
chenyangxi <chenyangxi@eswincomputing.com>
2026-01-29 10:17:47 +0800

    fix:fix npu mailbox bug
    Changelogs:
    	1、Fix the problem of mailbox write lock exception causing locked data overwrite
    
    Signed-off-by: chenyangxi <chenyangxi@eswincomputing.com>

--- drivers/soc/eswin/ai_driver/common/hetero_ipc.h
+++ drivers/soc/eswin/ai_driver/common/hetero_ipc.h
@@ -513,7 +513,7 @@ static void messagebox_send_p2p(msg_payload_t payload, u32 mbox_base)
 {
     u32 mbox_int, mbox_lock, timeout, data;
     mbox_int = BIT3;
-    mbox_lock = ((u32)payload.lparam) >> 8 + 16; // 1-16 reserved to u84 and dsp
+    mbox_lock = (((u32)payload.lparam) >> 8) + 16; // 1-16 reserved to u84 and dsp
     timeout = 0;
 
     // check lock bit and fifo
